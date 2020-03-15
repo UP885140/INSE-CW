64a57d9ad7ffc21cc7cdecd2243402b4c4b7770d
UP885140 <56303867+UP885140@users.noreply.github.com>
2020-03-15 15:40:21 +0000

    Add files via upload

---
+++ servlets/userloginServlet.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.mypackage.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import org.myhandler.userAccount.*;
+
+/**
+ *
+ * @author up885140
+ */
+@WebServlet(name = "userloginServlet", urlPatterns = {"/login"})
+public class userloginServlet extends HttpServlet {
+    
+    public userloginServlet(){
+        super();
+    }
+
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        
+        loginVerify loginverify = new loginVerify();
+        
+        try{
+            loginUser user = loginverify.checkLogin(email, password);
+            String destPage = "login.jsp";
+            
+            if(user != null){
+                HttpSession session = request.getSession();
+                session.setAttribute("user",user);
+                destPage = "home.jsp";
+            } else{
+                String message = "Invalid email/password";
+                request.setAttribute("message", message);
+            }
+            
+            RequestDispatcher dispatcher = request.getRequestDispatcher(destPage);
+            dispatcher.forward(request, response);
+        } catch (SQLException | ClassNotFoundException e){
+            throw new ServletException(e);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
